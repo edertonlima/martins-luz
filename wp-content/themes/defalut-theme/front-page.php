bcca76d5426c57a4afd1e5d8f43968008cb2c295
edertonlima <M0needer!@>
2020-06-09 16:51:00 -0300

    Itens destaque home
    conteudo direita/esquerda

--- wp-content/themes/defalut-theme/front-page.php
+++ wp-content/themes/defalut-theme/front-page.php
@@ -2,6 +2,52 @@
 
 	<?php while ( have_posts() ) : the_post(); ?>
 
+		<section class="box-section">
+			<div class="container">
+				<div class="row flex align-center">
+					
+					<div class="col-5">
+						<h2>
+							Reembolsos corporativos de forma simples e rápida.
+							<br>
+							<strong>Como deve ser.</strong>
+						</h2>
+
+						<p class="cont-recuo">Acabe com a dor de cabeça da gestão de despesas do dia a dia da sua empresa: pagamentos, reembolsos, compras on-line e gestão do fundo fixo. Controle seus gastos escaneando notas fiscais, acompanhando aprovações, visualizando relatórios e muito mais!</p>
+
+						<a href="" class="btn cor2" title="Peça já sua proposta">Peça já sua proposta</a>
+
+						<p class="cor2">Responderemos em até 24h.</p>
+					</div>
+					<div class="col-7">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/large-Header_3d_3_1.png" alt="Reembolsos corporativos de forma simples e rápida. Como deve ser">
+					</div>
+
+				</div>
+
+				<div class="row flex align-center">
+
+					<div class="col-7">
+						<a data-fancybox href="https://www.youtube.com/watch?v=OvtUAuyL6b4" class="video-home" title="Assistir ao vídeo">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/play-solid.png" alt="Assistir ao vídeo">
+						</a>
+					</div>
+					<div class="col-5">
+						<h2>
+							Assuma agora o 
+							<br>
+							<strong> controle dos gastos</strong>
+							<br>
+							variáveis da sua empresa
+						</h2>
+
+						<p>O BPP Corp é uma solução única para a administração de gastos corporativos que conta com uma conta digital empresarial grátis, cartões VISA pré-pagos para controle total das despesas e um aplicativo sem custos adicionais para tornar seu controle financeiro mais simples e prático! /p>
+					</div>
+
+				</div>
+			</div>
+		</section>
+
 	<?php endwhile; ?>
 
 <?php get_footer(); ?>
