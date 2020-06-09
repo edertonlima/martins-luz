eaaceeb309abd1f881b59372ed4f8395a6beadfa
edertonlima <M0needer!@>
2020-06-09 18:56:40 -0300

    Icones item page

--- wp-content/themes/defalut-theme/front-page.php
+++ wp-content/themes/defalut-theme/front-page.php
@@ -55,9 +55,9 @@
 					<div class="row flex align-bottom">
 
 						<div class="col-m-1 col-4">
-							<h2>
+							<h3>
 								<strong>Organizar<br>reembolsos</strong> da sua empresa nunca foi tão rápido
-							</h2>
+							</h3>
 
 							<p class="">Utilizando BPP Corp, você tem controle total sobre o budget de sua equipe podendo realizar transferências instantâneas para os cartões corporativos dos colaboradores, acompanhar gastos, receber comprovantes e visualizar relatórios que organizam seu reembolso.</p>
 
@@ -68,6 +68,61 @@
 						</div>
 
 					</div>
+				</div>
+
+			</div>
+		</section>
+
+		<section class="box-section">
+			<div class="container">
+
+				<div class="itens-ico">
+
+					<h3 class="center">
+						A plataforma que ajuda a <strong>reduzir os gastos</strong> do dia a dia
+					</h3>
+
+					<p class="subtitulo">Simplifique a gestão das despesas, evitando a perda de tempo com comprovantes e planilhas, reduzindo custos e aumentando os resultados da sua empresa.</p>
+
+						<div class="row">
+
+							<div class="col-4 item">
+								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico1.png" alt="Mais rapidez na gestão das suas despesas">
+								<h4>Mais rapidez na gestão das suas despesas</h4>
+								<p>Para que seu negócio cresça de maneira inteligente e rápida, substitua os processos demorados de reembolsos, aprovações, criação de relatórios e etc. por uma plataforma que te oferece tudo isso de forma rápida e digital. </p>
+							</div>
+
+							<div class="col-4 item">
+								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico2.png" alt="">
+								<h4>Os relatórios que você precisa em um único lugar</h4>
+								<p>Encontre relatórios detalhados sobre as utilizações de cada cartão corporativo cadastrado em sua equipe. Confira o saldo de cada cartão, redistribua valores, faça transferências instantâneas e não perca tempo com formulários e planilhas. </p>
+							</div>
+
+							<div class="col-4 item">
+								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico3.png" alt="">
+								<h4>Organize todos os seus comprovantes</h4>
+								<p>Recebendo os comprovantes de gastos de seus colaboradores através da plataforma, você não precisa mais guardar comprovantes e notas fiscais em lugar nenhum. Os gastos podem ser classificados por tipo ou por projeto, da forma que você precisar.</p>
+							</div>
+							<div class="col-4 item">
+								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico4.png" alt="">
+								<h4>Apresente suas despesas com um só clique</h4>
+								<p>Enviar seus comprovantes é mais simples do que você imagina. Basta que o colaborador tire uma foto do cupom fiscal com seu smartphone através do aplicativo BPP Card. O gestor das despesas pode aprovar ou reprovar aquele gasto através do App.</p>
+							</div>
+							<div class="col-4 item">
+								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico5.png" alt="">
+								<h4>Controle seus gastos com cartões pré-pagos VISA</h4>
+								<p>Os cartões pré-pago BPP Corp são fáceis de controlar: o colaborador só gasta o que a empresa disponibiliza para compras online ou em lojas físicas. Conte com cartões VISA aceitos em mais de 30 milhões de estabelecimentos no mundo todo, incluindo sites e aplicativos!</p>
+							</div>
+							<div class="col-4 item">
+								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ico6.png" alt="">
+								<h4>Conta digital gratuita e exclusiva para sua empresa</h4>
+								<p>Oferecemos uma conta digital gratuita especialmente para gerenciar despesas.  Transfira o saldo de sua conta para os cartões de maneira instantânea, sempre livre de taxas de manutenção ou de juros.</p>
+							</div>
+
+						</div>
+
+					</div>
+
 				</div>
 
 			</div>
